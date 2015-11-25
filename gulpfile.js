64cdc440aa8dee1be0c7559992e0f7529d478fa0
Ryan Dowling <ryan@ryandowling.me>
2015-11-25 20:51:58 +1100

    Switch to gulp and add in those necessary files for gulp, JSHint and JSCS

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+/*
+ * Allmighty Docker Manager - https://github.com/RyanTheAllmighty/Allmighty-Docker-Manager
+ * Copyright (C) 2015 RyanTheAllmighty
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function () {
+    'use strict';
+
+    let gulp = require('gulp');
+    let jscs = require('gulp-jscs');
+    let mocha = require('gulp-mocha');
+    let jshint = require('gulp-jshint');
+
+    gulp.task('jshint', function () {
+        return gulp.src(['app.js', 'inc/**/*.js'])
+            .pipe(jshint())
+            .pipe(jshint.reporter())
+            .pipe(jshint.reporter('fail'));
+    });
+
+    gulp.task('jscs', function () {
+        return gulp.src(['app.js', 'inc/**/*.js'])
+            .pipe(jscs())
+            .pipe(jscs.reporter())
+            .pipe(jscs.reporter('fail'));
+    });
+
+    gulp.task('test', function () {
+        return gulp.src('test/**/*.js')
+            .pipe(mocha({
+                reporter: 'spec',
+                clearRequireCache: true,
+                ignoreLeaks: true
+            }));
+    });
+
+    gulp.task('watch', function () {
+        gulp.watch(['app.js', 'inc/**/*.js', 'test/**/*.js'], ['jshint', 'jscs', 'test']);
+        gulp.start('default');
+    });
+
+    // The default task (called when you run `gulp` from cli)
+    gulp.task('default', ['jshint', 'jscs', 'test']);
+})();
